16fc2f34a2b95488b6917b4f5ea0dae7a35bed9f
Richard Dsouza <Richard Dsouza@DESKTOP-73SAJOK>
2023-06-05 14:41:43 +0800

    Add project files.

---
+++ EmpowerSG_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSG_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSG_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("25DCEF63-9B2A-4DBE-A9F4-920696656F2A")]
+[assembly: AssemblyVersion("1.0.0.0")]
